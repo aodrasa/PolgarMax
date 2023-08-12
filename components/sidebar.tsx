66439b0f43b58a0aec3ba28c2abadffa31a3474a
aminedjohar <aminedjohar@gmail.com>
2023-08-13 00:16:39 +0100

    add all items for sidebar and downgrade nextjs version for lucid-react icon works well

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 import { Montserrat } from "next/font/google";
 
 import { cn } from "@/lib/utils";
-import { LayoutDashboard } from 'lucide-react';
-const montserrat = Montserrat({ weight: "600", subsets:["latin"]});
+import { LayoutDashboard, MessageSquare, ImageIcon, VideoIcon, Music, Code, Settings } from 'lucide-react';
+const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
 const routes = [
     {
@@ -14,9 +14,43 @@ const routes = [
         icon: LayoutDashboard,
         href: "/dashboard",
         color: "text-sky-500",
-
-    }
-]
+    },
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        href: "/conversation",
+        color: "text-violet-500",
+    },
+    {
+        label: "Image Generation ",
+        icon: ImageIcon,
+        href: "/image",
+        color: "text-pink-700",
+    },
+    {
+        label: "Video Generation ",
+        icon: VideoIcon,
+        href: "/video",
+        color: "text-orange-700",
+    },
+    {
+        label: "Music Generation ",
+        icon: Music,
+        href: "/music",
+        color: "text-emerald-500",
+    },
+    {
+        label: "Code Generation ",
+        icon: Code,
+        href: "/code",
+        color: "text-green-700",
+    },
+    {
+        label: "Settings",
+        icon: Settings,
+        href: "/settings",
+    },
+];
 
 const Sidebar = () => {
     return (
@@ -24,16 +58,32 @@ const Sidebar = () => {
             <div className="px-3 py-2 flex-1">
                 <Link href="/dashboard" className="flex items-center pl-3 mb-14">
                     <div className="relative w-8 h-8 mr-4">
-                        <Image 
+                        <Image
                             fill
                             alt="Logo"
                             src="/logo.png"
                         />
                     </div>
                     <h1 className={cn("text-2xl font-bold", montserrat.className)}>
-                            PolgarMax
-                        </h1>
+                        PolgarMax
+                    </h1>
                 </Link>
+                <div className="space-y-1">
+                    {routes.map((route) => (
+                        <Link
+                            href={route.href}
+                            key={route.href}
+                            className="text-sm group flex p-3 w-full justify-start cursor-pointer hover:bg-white/10 rouded-lg transition"
+                        >
+                            <div className="flex items-center flex-1">
+                                <route.icon className={cn("h-5 w-5 mr-3", route, color)} />
+                                {route.label}
+
+                            </div>
+                        </Link>
+                    )
+                    )}
+                </div>
             </div>
         </div>
     );
