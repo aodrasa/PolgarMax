a813307a5c4599220560943a51fb39089adacee0
aminedjohar <aminedjohar@gmail.com>
2023-08-19 14:52:07 +0100

    started music generation page

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     images : {
         domains : [
-            "oaidalleapiprodscus.blod.core.windows.net",
+            "oaidalleapiprodscus.blob.core.windows.net",
         ]
     }
 }
